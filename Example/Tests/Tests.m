d5541ced578b3aa8e7c487dc0b8040081818ecbd
Naithar <devias.naith@gmail.com>
2015-04-26 14:09:48 +0300

    timer tests for zero interval and multiple run.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -72,6 +72,39 @@ describe(@"timer start", ^{
 
         expect(count).to.equal(25);
     });
+
+    it(@"will not run without interval", ^{
+        __block NSInteger count = 0;
+
+        waitUntil(^(DoneCallback done) {
+            [[[[[NHTimer timer] repeat:10] interval:0] timerBlock:^(NHTimer *timer) {
+                count++;
+            }] start];
+
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC * 3), dispatch_get_main_queue(), ^{
+                done();
+            });
+        });
+
+        expect(count).to.equal(0);
+    });
+
+    it(@"will not run until finished", ^{
+        __block NSInteger count = 0;
+
+        waitUntil(^(DoneCallback done) {
+            NHTimer *timer = [[[[[NHTimer timer] repeat:10] interval:0.1] timerBlock:^(NHTimer *timer) {
+                count++;
+            }] stopBlock:^(NHTimer *timer) {
+                done();
+            }];
+
+            [timer start];
+            [timer start];
+        });
+
+        expect(count).to.equal(10);
+    });
 });
 
 SpecEnd
